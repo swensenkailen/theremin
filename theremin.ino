f8cf4c72c90811d9abe2a113f5c3dcf001f6e9a5
swensenkailen <64510874+swensenkailen@users.noreply.github.com>
2022-04-27 00:22:58 -0700

    Added first file

---
+++ theremin.ino
@@ -0,0 +1,27 @@
+/**
+ * @author Kailen Swensen, Will McDonald
+ * 
+ * @brief  Arduino theremin with MIDI output
+ * 
+ * 
+ */
+
+void setup() 
+{
+  // set up sine wavetable (a bit more efficient than sin())
+  float sine_table[100];
+  
+  // set up any other variables
+  float frequency;
+}
+
+void loop()
+{
+  
+  // retrieve sensor data
+
+    // set oscillator frequency
+
+  // play sound
+    
+}
