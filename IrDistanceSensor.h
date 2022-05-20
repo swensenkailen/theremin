09f7c60c67bad5a6d3b695f38d8b807dd1893246
will-m-digipen <will.m@digipen.edu>
2022-05-19 17:44:04 -0700

    First Pass of IrDistanceSensor Class
    Implemented a class to simplify implementation of sensor data use.
    
    Sensor calibration can be configured using `#define`'s.  Default configuration is for Sharp GP2Y0A21YK0F IR Sensors.
    
    I do not have hardware on hand, so this has yet to be tested.  It is written based on my successfully tested `sensor-test`.

---
+++ IrDistanceSensor.h
@@ -0,0 +1,104 @@
+/**
+ * @file IrDistanceSensor.h
+ * @author Will McDonald
+ * @brief Interface Class for Sharp IR distance sensors
+ * @version 0.1
+ * @date 2022-05-19
+ * 
+ */
+
+// #include <math.h>
+
+#ifndef SMOOTHING_SAMPLES
+#define SMOOTHING_SAMPLES (5u)
+#endif
+
+#ifndef SENSOR_PERIOD
+#define SENSOR_PERIOD (40u) // minimum time allowed between samples
+#endif
+
+#ifndef SENSOR_COEFF
+#define SENSOR_COEFF (12.472f)
+#endif
+
+#ifndef SENSOR_EXPONENT
+#define SENSOR_EXPONENT (-1.068f)
+#endif
+
+#define ADC_TO_VOLTS 3.3f / ((0x01 << ADC_RESOLUTION)) // max voltage div by max val
+
+// NOTE: it is possible for the sum to overflow, if 1 << sizeof(int) - ADC_RESOLUTION is less than SMOOTHING_SAMPLE_COUNT.
+// this should not be the case, as RP2040 ADC_RESOLUTION = 12bits and int >= 16bits.  giving us more than enough samples for smoothing.
+
+// TODO: implement absolute/relative modes.
+
+class IrDistanceSensor
+{
+private:
+  int _pin = 0;
+  int samples[SMOOTHING_SAMPLES] = { 0 };
+  int curr_sample = 0;
+
+  unsigned long last_sample_millis = 0;
+
+  void storeSample(int sample)
+  {
+    samples[curr_sample++] = sample;
+
+    if (curr_sample >= SMOOTHING_SAMPLES)
+    {
+      curr_sample = 0;
+    }
+  }
+
+  int getSmoothed(void)
+  {
+    int sum = 0;
+
+    for (int i = 0; i < SMOOTHING_SAMPLES; ++i)
+    {
+      sum += samples[i];
+    }
+
+    return sum / SMOOTHING_SAMPLES;
+  }
+
+  int readADC(void)
+  {
+    // TODO: consider pushing/popping ADC resolution, Other libraries/ user code may be expecting a different value.
+    analogReadResolution(ADC_RESOLUTION);
+    return analogRead(_pin);
+  }
+
+public:
+  IrDistanceSensor(int pin) : _pin(pin)
+  {
+  }
+
+  void Init(void)
+  {
+    pinMode(_pin, INPUT);
+    analogReadResolution(ADC_RESOLUTION);
+
+    for (int i = 0; i < SMOOTHING_SAMPLES; ++i) {
+      storeSample(analogRead(_pin));
+      delay(SENSOR_PERIOD);
+    }
+  }
+
+  void sensorLoop(void)
+  {
+    unsigned long curr_millis = millis();
+
+    if (curr_millis - last_sample_millis > SENSOR_PERIOD)
+    {
+      storeSample(analogRead(_pin));
+      last_sample_millis = curr_millis;
+    }
+  }
+
+  float GetDistanceCM(void)
+  {
+    return SENSOR_COEFF * pow(getSmoothed() * ADC_TO_VOLTS, SENSOR_EXPONENT);
+  }
+};
--- theremin.ino
+++ theremin.ino
@@ -2,84 +2,62 @@
  * @author Kailen Swensen, Will McDonald
  * @date   May 2022
  * @brief  Arduino theremin with MIDI output
- *         NOTE: currently untested, nor debugged
- * 
+ * Written for RP2040
  */
 
- #include <math.h> // log()
- #define PI atan(1.0) * 4.0
+// #include <Button.h>
+#include "IrDistanceSensor.h"
 
-// While this comment exists, most values will be arbitrary until circuit is set up,
-// and pin values are certain
-const int ired_sensor1 = 1, 
-          ired_sensor2 = 2,
-          button1 = 3,
-          button2 = 4,
-          button3 = 5,
-          button4 = 6,
-          audio_out_pin = 7;
-          
-// if not too expensive, i'd like to use doubles, but can probably work with ints
-// TODO: set up sinewave lookup table (a bit more efficient than sin())
-const double sine_table[100];
-double frequency;
+#define PIN_SENS_L A0
+#define PIN_SENS_R A1
 
-void setup() 
+#define PIN_BUTTON_1 10
+#define PIN_BUTTON_2 11
+#define PIN_BUTTON_3 12
+#define PIN_BUTTON_4 13
+
+// Button button1(PIN_BUTTON_1),
+//        button2(PIN_BUTTON_2),
+//        button3(PIN_BUTTON_3),
+//        button4(PIN_BUTTON_4);
+
+
+#define LOGGING_DELAY 50
+int last_log_millis = 0;
+
+IrDistanceSensor sensor_left(PIN_SENS_L),
+                 sensor_right(PIN_SENS_R);
+
+void setup()
 {
-  // initialize serial communication:
-  Serial.begin(9600);
-  // set MIDI baud rate
-  Serial.begin(31250);
-  
-  // set up input/output pins
-  pinMode(ired_sensor1, INPUT);
-  pinMode(ired_sensor2, INPUT);
-  pinMode(button1, INPUT);
-  pinMode(button2, INPUT);
-  pinMode(button3, INPUT);
-  pinMode(button4, INPUT);
-  pinMode(audio_out_pin, OUTPUT);
+  // pinMode(PIN_SENS_L, INPUT);
+  // pinMode(PIN_SENS_R, INPUT);
+
+
+  // TODO: use Kails button library
+  // pinMode(PIN_BUTTON_1, INPUT_PULLUP);
+  // pinMode(PIN_BUTTON_2, INPUT_PULLUP);
+  // pinMode(PIN_BUTTON_3, INPUT_PULLUP);
+  // pinMode(PIN_BUTTON_4, INPUT_PULLUP);
+
+  SerialUSB.begin(9600);
+
+  sensor_left.Init();
+  sensor_right.Init();
 }
 
 void loop()
 {
-  // retrieve sensor/button data (will have to convert if data is given as ints)
-  double sensor1_data = pulseIn(ired_sensor1, HIGH),
-         sensor2_data = pulseIn(ired_sensor2, HIGH),
-         button1_data = pulseIn(button1, HIGH),
-         button2_data = pulseIn(button2, HIGH),
-         button3_data = pulseIn(button3, HIGH),
-         button4_data = pulseIn(button4, HIGH);
+  sensor_left.sensorLoop();
+  sensor_right.sensorLoop();
 
-  // set oscillator frequency, defaulted for now (will use sensor_data)
-  int carrier_frequency = 440;    
-  int mopdulator_frequency = 220; 
+  unsigned long curr_millis = millis();
+  if (curr_millis - last_log_millis > LOGGING_DELAY) {
+    SerialUSB.print("Left Sensor: ");
+    SerialUSB.println(sensor_left.GetDistanceCM());
+    SerialUSB.print("Right Sensor: ");
+    SerialUSB.println(sensor_right.GetDistanceCM());
+  }
 
-  // this is where we'll put the final calculated output,
-  // currently not too sure if this needs to be an int for analog output
-  int audio_output = 0;
-
-  // 144 = note on
-  int midi_command = 0x90;
-  // frequency to midi conversion formula
-  int midi_note = (int)((12 * log(frequency/220.0) / log(2.0)) + 57);
-  // velocity, could be controlled by button_data or second sensor if wanted
-  int midi_velocity = 0;
-    
-  // play sound
-  analogWrite(audio_out_pin, audio_output);
-  
-  // alternatively, send MIDI
-  midiWrite(midi_command, midi_note, midi_velocity);
-
-  // update viewport
-  Serial.print("Theremin");
-}
-
-void midiWrite(int command, int note, int velocity)
-{
-  // write the command, note value, and velocity value to MIDI output
-  Serial.write(command);
-  Serial.write(note);
-  Serial.write(velocity);
+  delay(10);
 }
