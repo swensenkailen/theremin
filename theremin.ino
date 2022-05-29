e6cf5f62511609586c56bd8364272236479c8bc5
swensenkailen <64510874+swensenkailen@users.noreply.github.com>
2022-05-29 16:38:43 -0700

    Updated theremin
    Updated theremin using Tonesynth class

--- theremin.ino
+++ theremin.ino
@@ -1,85 +1,77 @@
 /**
  * @author Kailen Swensen, Will McDonald
  * @date   May 2022
- * @brief  Arduino theremin with MIDI output
- *         NOTE: currently untested, nor debugged
- * 
+ * @brief  Arduino theremin with Audio/MIDI output
+ * Written for RP2040
  */
 
- #include <math.h> // log()
- #define PI atan(1.0) * 4.0
+#include <Button.h>
+#include "IrDistanceSensor.h"
+#include "ToneSynth.h"
 
-// While this comment exists, most values will be arbitrary until circuit is set up,
-// and pin values are certain
-const int ired_sensor1 = 1, 
-          ired_sensor2 = 2,
-          button1 = 3,
-          button2 = 4,
-          button3 = 5,
-          button4 = 6,
-          audio_out_pin = 7;
-          
-// if not too expensive, i'd like to use doubles, but can probably work with ints
-// TODO: set up sinewave lookup table (a bit more efficient than sin())
-const double sine_table[100];
-double frequency;
+#define PIN_SENS_L A0
+#define PIN_SENS_R A1
 
-void setup() 
+#define AUDIO_OUT_L 2
+#define AUDIO_OUT_R 3
+
+#define PIN_BUTTON_1 10
+#define PIN_BUTTON_2 11
+#define PIN_BUTTON_3 12
+#define PIN_BUTTON_4 13
+
+Button button1(PIN_BUTTON_1),
+       button2(PIN_BUTTON_2),
+       button3(PIN_BUTTON_3),
+       button4(PIN_BUTTON_4);
+
+ToneSynth synth;
+
+#define LOGGING_DELAY 50
+int last_log_millis = 0;
+
+IrDistanceSensor_Relative sensor_left(PIN_SENS_L),
+                          sensor_right(PIN_SENS_R);
+
+void setup()
 {
-  // initialize serial communication:
-  Serial.begin(9600);
-  // set MIDI baud rate
-  Serial.begin(31250);
-  
-  // set up input/output pins
-  pinMode(ired_sensor1, INPUT);
-  pinMode(ired_sensor2, INPUT);
-  pinMode(button1, INPUT);
-  pinMode(button2, INPUT);
-  pinMode(button3, INPUT);
-  pinMode(button4, INPUT);
-  pinMode(audio_out_pin, OUTPUT);
+  synth.setPins(AUDIO_OUT_L, AUDIO_OUT_R);
+  synth.setDelay(10);
+  synth.setMaxFrequency(1500);
+  // TODO: use Kails button library
+  // pinMode(PIN_BUTTON_1, INPUT_PULLUP);
+  // pinMode(PIN_BUTTON_2, INPUT_PULLUP);
+  // pinMode(PIN_BUTTON_3, INPUT_PULLUP);a
+  // pinMode(PIN_BUTTON_4, INPUT_PULLUP);
+
+  SerialUSB.begin(9600);
+
+  sensor_left.Init();
+  sensor_right.Init();
 }
 
 void loop()
 {
-  // retrieve sensor/button data (will have to convert if data is given as ints)
-  double sensor1_data = pulseIn(ired_sensor1, HIGH),
-         sensor2_data = pulseIn(ired_sensor2, HIGH),
-         button1_data = pulseIn(button1, HIGH),
-         button2_data = pulseIn(button2, HIGH),
-         button3_data = pulseIn(button3, HIGH),
-         button4_data = pulseIn(button4, HIGH);
+  sensor_left.sensorLoop();
+  sensor_right.sensorLoop();
 
-  // set oscillator frequency, defaulted for now (will use sensor_data)
-  int carrier_frequency = 440;    
-  int mopdulator_frequency = 220; 
+  // set delay based on left sensor distance
+  synth.setDelay(fmap(sensor_left.GetDistance(), 0.0f, 1.0f, 10.0f, 60.0f));
+  // set frequency based on left sensor distance
+  synth.setFrequency(fmap(sensor_right.GetDistance(), 
+                     0.0f, 1.0f, 110.0f, 1760.0f));
 
-  // this is where we'll put the final calculated output,
-  // currently not too sure if this needs to be an int for analog output
-  int audio_output = 0;
+  unsigned long curr_millis = millis();
+  if (curr_millis - last_log_millis > LOGGING_DELAY) 
+  {
 
-  // 144 = note on
-  int midi_command = 0x90;
-  // frequency to midi conversion formula
-  int midi_note = (int)((12 * log(frequency/220.0) / log(2.0)) + 57);
-  // velocity, could be controlled by button_data or second sensor if wanted
-  int midi_velocity = 0;
-    
-  // play sound
-  analogWrite(audio_out_pin, audio_output);
-  
-  // alternatively, send MIDI
-  midiWrite(midi_command, midi_note, midi_velocity);
+    SerialUSB.print("Left Sensor: ");
+    //SerialUSB.println(sensor_left.GetDistance());
+    SerialUSB.print("Right Sensor: ");
+    SerialUSB.println(sensor_right.GetDistance());
+  }
 
-  // update viewport
-  Serial.print("Theremin");
-}
+  synth.update();
 
-void midiWrite(int command, int note, int velocity)
-{
-  // write the command, note value, and velocity value to MIDI output
-  Serial.write(command);
-  Serial.write(note);
-  Serial.write(velocity);
+  //delay(10);
 }
